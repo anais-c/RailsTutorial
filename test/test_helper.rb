f63fc37e1f7e8287cba0f1cc048c5ae7f79c92bb
Anais <anaiskat@gmail.com>
2015-06-18 22:37:00 +0000

    Finish log in/log out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,30 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  
+  # Returns true if a test user is logged in. Los helpers normales no estan disponibles para los test, como por ejemplo,
+  # el logged_in?, así que tenemos que crear helpers que hagan la misma función solo para los tests.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end  
 
-  # Add more helper methods to be used by all tests here...
+  # Logs in a test user.
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    # Returns true inside an integration test.
+    def integration_test?
+      defined?(post_via_redirect)
+    end  
 end
