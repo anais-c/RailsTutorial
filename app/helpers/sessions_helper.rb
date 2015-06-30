57c46d21e5ec7bf13c382a881f5e03c237a92c1e
Anais <anaiskat@gmail.com>
2015-06-30 15:20:51 +0000

    Finish user edit, update, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end    
+  
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end  
     
   # Returns the current logged-in user (if any).
   def current_user
@@ -30,6 +35,13 @@ module SessionsHelper
       !current_user.nil?
   end
 
+  #Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Logs out the current user.    
   def log_out
     forget(current_user)    
@@ -37,4 +49,14 @@ module SessionsHelper
     @current_user = nil
   end
     
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end    
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,7 +19,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid information followed by logout" do
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
-    assert is_logged_in?  
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
@@ -30,12 +30,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not is_logged_in?
     assert_redirected_to root_url
     # Simulate a user clicking logout in a second window.
-    delete logout_pathe    
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0    
-  end  
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
   
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
